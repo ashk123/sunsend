fbd4df052089223f0f0b2aa3239aea09a1f5dc88
ashk123 <ashkqwe123ww@gmail.com>
2024-02-12 12:39:25 +0330

    add OpenArchive to open archive files

--- internals/Base/Archive.go
+++ internals/Base/Archive.go
@@ -1,9 +1,13 @@
 package Base
 
 import (
+	"errors"
 	"fmt"
-	"math/rand"
-	"sunsend/internals/CoreConfig"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+	"sunsend/internals/Data"
 	"time"
 
 	"github.com/klauspost/compress/zstd"
@@ -11,88 +15,95 @@ import (
 
 /*
 	The main structue of this API is the server will encode the old
-	messages and archive them inside the program and if these message
-	Will not call again, server will remove these message forever.
+	messages and archive them inside the program for saving it as zstd
+	compression, and if these message Will not call again, server will archive/remove
+	These messages forever.
 
 	Archiving messages is a new way of saving data when system
 	has more than a lot of data (more than limit) or users don't want their messages.
-	Those message will removed forever if some conditions are true.
+	Those message will removed forever or archive as a encoded data
+	If some conditions will be true.
 
 	- There are 3 condition for deleting files
-		1. Each 5 minute, server will try to get some old data by their Date
-		And if user don't want to remove it, those data will lose forever.
-		2. When user delete a message that message will be a archive encoding data
-		And only remve when user want to remove it completely.
-		3. manager of server can do this steps manually
+	1. Each 5 minute, server will try to get some old data by their Date
+		And if user don't want to remove it, those data will archive/lose forever.
+	2. When user delete a message that message will be a archive encoding data
+		And only remove when user want to remove it completely.
+	3. manager of server can do these steps manually
 
 	You can turn on or off this feature in the configuration file
 	If you wanna know more about the Archiving feature
 	Read the Archive.md documentation.
 */
 
-type ArchiveMsg struct {
-	ID      int
-	Content []byte
-	Date    string
+type ArchiveMessage struct {
+	A_MID     int
+	A_Content string
+	A_Date    string
 }
-
-type Archive struct {
-	Last   ArchiveMsg
-	Data   []ArchiveMsg // we can use map instead of slice too
-	Length int          // this will be something like int32 or in64 in the future
-}
-
-var archive *Archive // Main archive object
 
 func Compress(src []byte) []byte {
 	var encoder, _ = zstd.NewWriter(nil, zstd.WithEncoderLevel(zstd.SpeedBestCompression))
 	return encoder.EncodeAll(src, make([]byte, 0, len(src)))
 }
+func Decompress(src []byte) ([]byte, error) {
+	var decoder, _ = zstd.NewReader(nil, zstd.WithDecoderConcurrency(0))
+	return decoder.DecodeAll(src, nil)
+}
 
-func (p *Archive) AddArchive(data string) {
-	encoded := Compress([]byte(data))
-	gen_archive_msg := ArchiveMsg{
-		ID:      rand.Intn(10),
-		Content: encoded,
-		Date:    fmt.Sprintf("%d/%d/%d", time.Now().Hour(), time.Now().Weekday(), time.Now().Day()),
+func AddArchive(message *Data.Message) {
+	var res_ID int = message.MID
+	var res_Content string = message.Content
+	var res_Date string = message.Date
+	encoded_text_org := Itsr(res_ID) + "-" + res_Content + "-" + res_Date + "+"
+	encoded_res := Compress([]byte(encoded_text_org))
+	f, _ := os.OpenFile("Archive_"+fmt.Sprintf("%d_%d_%d", time.Now().Year(), time.Now().Weekday(), time.Now().Day())+".arc", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	defer f.Close()
+	f.Write(encoded_res)
+	log.Println("System Created a archive file of old messages")
+}
+
+func OpenArchive(date string) ([]*ArchiveMessage, error) {
+	var result []*ArchiveMessage
+	f, err := os.ReadFile("Archive_" + date + ".arc")
+	if err != nil {
+		fmt.Println(err.Error())
+		return nil, errors.New("there is not any Archive_" + date + ".arc file!")
 	}
-	p.Last = gen_archive_msg
-	p.Length = p.Length + 1 // add new msgs to the length of the Datas slice
-	p.Data = append(p.Data, gen_archive_msg)
-}
-
-func (p *Archive) RemoveArchive(ID int) {
-	for _, v := range p.Data {
-		if v.ID == ID {
-			// remove item from here
-			fmt.Println(v)
-		}
+	// fmt.Println(f)
+	res, err := Decompress(f)
+	if err != nil {
+		fmt.Println(err.Error())
+		return nil, errors.New("can't compress the archive file")
 	}
-}
+	data := string(res)
 
-func (p *Archive) GetLength() int {
-	return p.Length
-}
-
-func (p *Archive) GetLast() *ArchiveMsg {
-	return &p.Last
-}
-
-func getArchiveObj() *Archive {
-	if CoreConfig.Configs.Bin == true {
-		return archive
-	} else {
-		return nil
+	// *result* slice can be a simple map, to be faster
+	for _, v := range strings.Split(data[:len(data)-1], "+") {
+		sub_data := strings.Split(v, "-")
+		user_MID, _ := strconv.Atoi(sub_data[0])
+		result = append(result, &ArchiveMessage{
+			A_MID:     user_MID,
+			A_Content: sub_data[1],
+			A_Date:    sub_data[2],
+		})
 	}
+	if len(result) < 1 {
+		return nil, errors.New("there is not any data in the slice")
+	}
+	return result, nil
 }
 
-func (p *Archive) SaveArchiveMessages() int {
-	// save the messages with zstd format to the server
-	return 0 // result status code
-}
-
-func (p *Archive) UpdateMessages() {
-	// get old messages from database and operate on them
-	// SaveArchiveMessages will call in this function
-	// Let's do some cool things ;D
-}
+// // Generate New archive object, if there is not any object
+// func SetArchiveObj() *Archive {
+// 	if archive == nil {
+// 		archive = &Archive{
+// 			Last:   nil,
+// 			Data:   []*Data.Message{},
+// 			Length: 0,
+// 		}
+// 		return archive
+// 	} else {
+// 		return archive
+// 	}
+// }
--- tests/encoder.go
+++ tests/encoder.go
@@ -2,28 +2,93 @@ package main
 
 import (
 	"fmt"
+	"sunsend/internals/Base"
 
 	"github.com/klauspost/compress/zstd"
 )
+
+func Decompress(src []byte) ([]byte, error) {
+	var decoder, _ = zstd.NewReader(nil, zstd.WithDecoderConcurrency(0))
+	return decoder.DecodeAll(src, nil)
+}
 
 // This is not a main test file, it only works for testing libraries
 // And it will not be on release versions
 // Ignore this file if you wanna fork the project
 func main() {
-	var encoded []byte = Compress([]byte("welcome"))
-	decoded, _ := Decompress(encoded)
-	fmt.Println("Encoded version of that string:", encoded)
-	fmt.Println("UTF-8 version of that encoded:", string(encoded))
-	fmt.Println("Main Byte of that string:", []byte("welcome"))
-	fmt.Printf(string(decoded))
-}
+	// var encoded []byte = Compress([]byte("welcome"))
+	// decoded, _ := Decompress(encoded)
+	// fmt.Println("Encoded version of that string:", encoded)
+	// fmt.Println("UTF-8 version of that encoded:", string(encoded))
+	// fmt.Println("Main Byte of that string:", []byte("welcome"))
+	// fmt.Printf(string(decoded))
+	// msg := &Data.Message{
+	// 	CID:     123,
+	// 	MID:     124123,
+	// 	Author:  "eiko",
+	// 	Content: "welcome",
+	// 	Date:    "nice",
+	// 	ReplyID: 0,
+	// }
+	// Base.AddArchive(msg)
+	// msg2 := &Data.Message{
+	// 	CID:     123,
+	// 	MID:     124123,
+	// 	Author:  "eiko",
+	// 	Content: "welcome",
+	// 	Date:    "nice",
+	// 	ReplyID: 0,
+	// }
+	// Base.AddArchive(msg2)
+	// msg3 := &Data.Message{
+	// 	CID:     123,
+	// 	MID:     124123,
+	// 	Author:  "eiko",
+	// 	Content: "welcome",
+	// 	Date:    "nice",
+	// 	ReplyID: 0,
+	// }
+	// Base.AddArchive(msg3)
+	// msg4 := &Data.Message{
+	// 	CID:     123,
+	// 	MID:     124123,
+	// 	Author:  "eiko",
+	// 	Content: "welcome",
+	// 	Date:    "nice",
+	// 	ReplyID: 0,
+	// }
+	// Base.AddArchive(msg4)
 
-func Compress(src []byte) []byte {
-	var encoder, _ = zstd.NewWriter(nil, zstd.WithEncoderLevel(zstd.SpeedBestCompression))
-	return encoder.EncodeAll(src, make([]byte, 0, len(src)))
-}
+	res, err := Base.OpenArchive("2024_1_12")
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+	for i, v := range res {
+		fmt.Println(i, v)
+	}
 
-func Decompress(src []byte) ([]byte, error) {
-	var decoder, _ = zstd.NewReader(nil, zstd.WithDecoderConcurrency(0))
-	return decoder.DecodeAll(src, nil)
+	// defer f.Close()
+	// // v, _ := r4.ReadByte()
+	// // fmt.Println(v)
+	// // v2, _ := r4.ReadByte()
+	// // fmt.Println(v2)
+	// var org []string
+	// for {
+	// 	value, err := r4.ReadSlice(byte('+'))
+	// 	decoded_str_org, err := Decompress(value)
+	// 	if err != nil {
+	// 		fmt.Println(err.Error())
+	// 	}
+	// 	// org = append(org, string(value[:len(string(value))-1]))
+	// 	org = append(org, string(decoded_str_org))
+	// 	if err != nil {
+	// 		break
+	// 	}
+
+	// 	// fmt.Println(r4.ReadSlice(byte('+')))
+	// 	// fmt.Println(r4.ReadSlice(byte('+')))
+	// }
+	// for index, value := range org {
+	// 	fmt.Println(index, value)
+	// }
 }
